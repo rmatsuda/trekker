759d426d298db487dfc58acc75ff19af4b42dc74
Renan <renan_hiroshi@hotmail.com>
2026-04-24 07:33:54 +0300

    Refactor initialize function in Trekker.pyx
    Updated the initialization function to raise ValueError for missing FOD image path and invalid argument count.

--- src/python/cython/Trekker.pyx
+++ src/python/cython/Trekker.pyx
@@ -272,22 +272,21 @@ cdef object PyTrekker(Trekker *Trekker_ptr):
 
 def initialize(pathToFODimage=None, arg1=None, arg2=None, arg3=None, arg4=None):
 
-    if (isInitialized()==True):
-        raise Exception('Trekker is already initialized. To initialize with a new FOD image, first delete your existing Trekker object with \"del <variable_name>\" ')
+    if pathToFODimage is None:
+        raise ValueError("Provide FOD image path")
 
     cdef Trekker *_ptr = NULL
-#
-    """Factory function"""
-    if (pathToFODimage is None):
-        print("Provide FOD image path")
-    elif (arg1 is None) and (arg2 is None) and (arg3 is None) and (arg4 is None):
-        _ptr = new Trekker(pathToFODimage)
-    elif (arg1 is not None) and (arg2 is not None) and (arg3 is None) and (arg4 is None):
-        _ptr = new Trekker(pathToFODimage,arg1,arg2)
-    elif (arg1 is not None) and (arg2 is not None) and (arg3 is not None) and (arg4 is not None):
-        _ptr = new Trekker(pathToFODimage,arg1,arg2,arg3,arg4)
-    else:
-        raise Exception('Invalid number of arguments used for initialization.')
 
-    cdef T m = PyTrekker(_ptr)
-    return m
+    if arg1 is None and arg2 is None and arg3 is None and arg4 is None:
+        _ptr = new Trekker(pathToFODimage)
+
+    elif arg1 is not None and arg2 is not None and arg3 is None:
+        _ptr = new Trekker(pathToFODimage, arg1, arg2)
+
+    elif arg3 is not None and arg4 is not None:
+        _ptr = new Trekker(pathToFODimage, arg1, arg2, arg3, arg4)
+
+    else:
+        raise ValueError("Invalid number of arguments")
+
+    return PyTrekker(_ptr)
